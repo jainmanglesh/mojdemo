510b8a3a58c5d073ce3143cda970ef5bdf9f4bb5
pg02182 <pg02182@VDI-PG02182.rci.uk>
2019-02-05 00:46:20 +0000

    Initial commit with springboot starter file and its test.

---
+++ src/main/java/com/test/mojdemo/MojApplication.java
@@ -0,0 +1,18 @@
+package com.test.mojdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Manglesh Jain
+ * Main Spring boot application class
+ */
+@SpringBootApplication
+public class MojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojApplication.class, args);
+	}
+
+}
+
